fe05eca244ad92849dd9bc43bbb62425e419c006
Riccardo Camuffo <riccardo.camuffo@gmail.com>
2021-09-28 15:17:06 +0200

    Rimessa la chiamata al DB

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 echo "GIT - Test<br>";
 echo "Modifica. Vediamo se arriva su da sola";
 
-/*
 try {
     $dbh = new PDO('mysql:host=192.168.10.25;dbname=test', $user, $pass);
     foreach($dbh->query('SELECT * from FOO') as $row) {
@@ -14,6 +13,5 @@ try {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
- */
 
 ?>
