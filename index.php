7ee3d1479e3217f76ca870451eae1450839ab458
Riccardo Camuffo <riccardo.camuffo@gmail.com>
2021-09-28 15:12:00 +0200

    Gateway timeout

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 echo "GIT - Test<br>";
 echo "Modifica. Vediamo se arriva su da sola";
 
+/*
 try {
     $dbh = new PDO('mysql:host=192.168.10.25;dbname=test', $user, $pass);
     foreach($dbh->query('SELECT * from FOO') as $row) {
@@ -13,5 +14,6 @@ try {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
+ */
 
 ?>
