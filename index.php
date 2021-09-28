f9c0bd15881b4c0877d17c8c82cba47a7e02e3d4
Riccardo Camuffo <riccardo.camuffo@gmail.com>
2021-09-28 16:22:14 +0200

    Prova

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ echo "GIT - Test<br>";
 echo "Modifica. Vediamo se arriva su da sola";
 
 try {
-    $dbh = new PDO('mysql:host=192.168.10.25;dbname=test', $user, $pass);
+    $user = 'pgmd';
+    $pass = 'pgmdpmgd!';
+
+    $dbh = new PDO('mysql:host=192.168.10.25;dbname=pentaho', $user, $pass);
     foreach($dbh->query('SELECT * from FOO') as $row) {
         print_r($row);
     }
