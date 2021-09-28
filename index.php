ae8bac7da190e5b14d4f3fad0e44b3678034e2b0
Riccardo Camuffo <riccardo.camuffo@gmail.com>
2021-09-28 13:35:58 +0200

    Prova di connessione ad DB farlocca

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
-	echo "GIT - Test<br>";
-	echo "Modifica. Vediamo se arriva su da sola";
+echo "GIT - Test<br>";
+echo "Modifica. Vediamo se arriva su da sola";
+
+try {
+    $dbh = new PDO('mysql:host=192.168.19.25;dbname=test', $user, $pass);
+    foreach($dbh->query('SELECT * from FOO') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
 
 ?>
