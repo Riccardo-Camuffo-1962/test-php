d9b5f045d8ab58398dd84bb6561503c48c1588ae
Riccardo Camuffo <riccardo.camuffo@gmail.com>
2021-10-07 10:53:23 +0200

    Prova

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 	echo "Siamo solo noi<br>";
-	echo "Forse.... - Trigger";
-	echo "Forse.... - Trigger";
+	echo "Forse.... - Trigger<br>";
+	echo "Forse.... - Trigger<br>";
+	echo "Effetto demo Wally";
 
 
 ?>
