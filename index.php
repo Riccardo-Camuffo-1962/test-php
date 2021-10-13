cb41a125e76831f25581a7d491b00ad86293f205
Riccardo Camuffo <riccardo.camuffo@gmail.com>
2021-10-13 17:51:22 +0200

    Curlo 4

--- index.php
+++ index.php
@@ -5,15 +5,17 @@
 	echo "Effetto demo Wally<br>";
 
 	$ch = curl_init();
-	curl_setopt( $ch, CURLOPT_URL, $url );
+	curl_setopt( $ch, CURLOPT_URL, 'http://192.168.10.25' );
 	curl_setopt( $ch, CURLOPT_PORT, 2424 );
 	curl_setopt( $ch, CURLOPT_TIMEOUT, 400 );
 	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, TRUE );
 	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
 
 	$report=curl_getinfo($ch);
+	echo "\n";
 	print_r($report);
 	$result = curl_exec($ch);
+	echo $result."\n";
   	echo curl_error($ch);
 
 	curl_close($ch);
