779152bcb9153424a5b610b2c33ff226c3609816
Riccardo Camuffo <riccardo.camuffo@gmail.com>
2021-10-13 17:41:57 +0200

    Curlo

--- index.php
+++ index.php
@@ -2,7 +2,19 @@
 	echo "Siamo solo noi<br>";
 	echo "Forse.... - Trigger<br>";
 	echo "Forse.... - Trigger<br>";
-	echo "Effetto demo Wally";
+	echo "Effetto demo Wally<br>";
 
+	$ch = curl_init();
+	curl_setopt( $ch, CURLOPT_URL, $url );
+	curl_setopt( $ch, CURLOPT_PORT, 2424 );
+	curl_setopt( $ch, CURLOPT_TIMEOUT, 400 );
+	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, TRUE );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
+
+	$report=curl_getinfo($ch);
+	print_r($report);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	echo $result;
 
 ?>
